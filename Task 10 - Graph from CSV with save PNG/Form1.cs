31035c18de15444da8ba21396159702927ac3354
David Petticrew <cardassianscot@gmail.com>
2018-03-28 10:35:18 +0100

    Add project files.

---
+++ Task 10 - Graph from CSV with save PNG/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_10___Graph_from_CSV_with_save_PNG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
